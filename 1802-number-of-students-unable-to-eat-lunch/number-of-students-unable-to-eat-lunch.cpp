874cd6e8e43979b21efac46417ab37520db42e8d
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-04-08 12:21:13 +0530

    Time: 5 ms (31.29%) | Memory: 10.7 MB (79.46%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        queue<int>q;
-        for(auto it:students){
-            q.push(it);
-        }
+        // using queue space
+        // queue<int>q;
+        // for(auto it:students){
+        //     q.push(it);
+        // }
+        // int cnt=0;
+        // while(true){
+        //     if(sandwiches[0]!=q.front()){
+        //         int top=q.front();
+        //         q.pop();
+        //         q.push(top);
+        //         cnt++;
+        //     }
+        //     else{
+        //         q.pop();
+        //         sandwiches.erase(sandwiches.begin()+0);
+        //         cnt=0;
+        //     }
+        //     if(cnt==q.size())break;
+        // }
+        // return cnt;
+        // without queue
         int cnt=0;
         while(true){
-            if(sandwiches[0]!=q.front()){
-                int top=q.front();
-                q.pop();
-                q.push(top);
+            if(sandwiches[0]!=students[0]){
+                int top=students[0];
+                students.erase(students.begin()+0);
+                students.push_back(top);
                 cnt++;
             }
             else{
-                q.pop();
+                students.erase(students.begin()+0);
                 sandwiches.erase(sandwiches.begin()+0);
                 cnt=0;
             }
-            if(cnt==q.size())break;
+            if(cnt==students.size())break;
         }
         return cnt;
     }
