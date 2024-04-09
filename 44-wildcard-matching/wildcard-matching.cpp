cc68b4127ad7ab038837c4e05882c773e4c2c1a2
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-04-09 12:58:28 +0530

    Time: 55 ms (52.74%) | Memory: 11.2 MB (88.70%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -22,9 +22,66 @@ class Solution {
     }
 public:
     bool isMatch(string s, string p) {
-        int n=s.size()-1;
-        int m=p.size()-1;
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solve(s,p,n,m,dp);
+        int n=s.size();
+        int m=p.size();
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
+        // return solve(s,p,n,m,dp);
+        // bottom up
+        // vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+        // dp[0][0]=true;
+        // for(int i=1;i<=n;i++){
+        //    dp[i][0]=false;
+        // }
+        // for(int j=1;j<=m;j++){
+        //     bool flag=true;
+        //     for(int k=1;k<=j;k++){
+        //         if(p[k-1]!='*'){
+        //             flag=false;
+        //             break;
+        //         }
+        //     }
+        //     dp[0][j]=flag;
+        // }
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(p[j-1]==s[i-1] || p[j-1]=='?'){
+        //             dp[i][j]=dp[i-1][j-1];
+        //         }
+        //         else if(p[j-1]=='*'){
+        //             dp[i][j]=dp[i-1][j]|dp[i][j-1];
+        //         }
+        //         else{
+        //             dp[i][j]=false;
+        //         }
+        //     }
+        // }
+        // return dp[n][m];
+        vector<bool>prev(m+1,false),curr(m+1,false);
+        prev[0]=true;
+        for(int i=1;i<=m;i++){
+            bool flag=true;
+            for(int k=1;k<=i;k++){
+                if(p[k-1]!='*'){
+                    flag=false;
+                    break;
+                }
+            }
+            prev[i]=flag;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(p[j-1]==s[i-1] || p[j-1]=='?'){
+                   curr[j]=prev[j-1];
+                }
+                else if(p[j-1]=='*'){
+                    curr[j]=prev[j]|curr[j-1];
+                }
+                else{
+                    curr[j]=false;
+                }
+            }
+            prev=curr;
+        }
+        return prev[m];
     }
 };
