95a881e3128372b6b5eb4fbd7f3af39272f2ed9e
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-04-11 15:27:41 +0530

    Time: 81 ms (64.86%) | Memory: 57.4 MB (80.51%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -18,37 +18,37 @@ public:
         // vector<vector<int>>dp(n,vector<int>(2,-1));
         // return solve(prices,fee,0,1,dp);
         // tabulation
-        vector<vector<int>>dp(n+1,vector<int>(2,0));
-        // no need to write base case as prefilled in dp
-        for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0;buy<=1;buy++){
-                int profit=0;
-                if(buy){
-                    profit=max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
-                }
-                else{
-                    profit=max((prices[ind]-fee)+dp[ind+1][1],dp[ind+1][0]);
-                }
-            dp[ind][buy]=profit;
-            }
-        }
-        return dp[0][1];
-
-        // space optimisation
-        // vector<int>prev(2,0),curr(2,0);
+        // vector<vector<int>>dp(n+1,vector<int>(2,0));
+        // // no need to write base case as prefilled in dp
         // for(int ind=n-1;ind>=0;ind--){
         //     for(int buy=0;buy<=1;buy++){
         //         int profit=0;
         //         if(buy){
-        //             profit=max(-prices[ind]+prev[0],prev[1]);
+        //             profit=max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
         //         }
         //         else{
-        //             profit=max((prices[ind]-fee)+prev[1],prev[0]);
+        //             profit=max((prices[ind]-fee)+dp[ind+1][1],dp[ind+1][0]);
         //         }
-        //     curr[buy]=profit;
+        //     dp[ind][buy]=profit;
         //     }
-        //     prev=curr;
         // }
-        // return prev[1];
+        // return dp[0][1];
+
+        // space optimisation
+        vector<int>prev(2,0),curr(2,0);
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                int profit=0;
+                if(buy){
+                    profit=max(-prices[ind]+prev[0],prev[1]);
+                }
+                else{
+                    profit=max((prices[ind]-fee)+prev[1],prev[0]);
+                }
+            curr[buy]=profit;
+            }
+            prev=curr;
+        }
+        return prev[1];
     }
 };
