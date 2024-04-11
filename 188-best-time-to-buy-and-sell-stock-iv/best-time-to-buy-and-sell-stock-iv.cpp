7088f896444bed8e44c1f7669ee2e1f3e046a81d
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-04-11 14:04:02 +0530

    Time: 3 ms (92.51%) | Memory: 13.2 MB (75.34%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -18,22 +18,41 @@ public:
         int n=prices.size();
         // vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
         // return solve(prices,n,0,k,1,dp);
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        // // no need fill the base cases as initially it is zero
+        // for(int ind=n-1;ind>=0;ind--){
+        //     for(int buy=0;buy<=1;buy++){
+        //         for(int j=1;j<=k;j++){
+                
+        //         if(buy){
+        //             dp[ind][buy][j]=max(-prices[ind]+dp[ind+1][0][j],0+dp[ind+1][1][j]);
+        //         }
+        //         else{
+        //             dp[ind][buy][j]=max(prices[ind]+dp[ind+1][1][j-1],0+dp[ind+1][0][j]);
+        //         }
+
+        //         }
+        //     }
+        // }
+        // return dp[0][1][k];
+
+        // space optimisation
+        vector<vector<int>>prev(2,vector<int>(k+1,0)),curr(2,vector<int>(k+1,0));
         // no need fill the base cases as initially it is zero
         for(int ind=n-1;ind>=0;ind--){
             for(int buy=0;buy<=1;buy++){
                 for(int j=1;j<=k;j++){
                 
                 if(buy){
-                    dp[ind][buy][j]=max(-prices[ind]+dp[ind+1][0][j],0+dp[ind+1][1][j]);
+                    curr[buy][j]=max(-prices[ind]+prev[0][j],0+prev[1][j]);
                 }
                 else{
-                    dp[ind][buy][j]=max(prices[ind]+dp[ind+1][1][j-1],0+dp[ind+1][0][j]);
+                    curr[buy][j]=max(prices[ind]+prev[1][j-1],0+prev[0][j]);
                 }
-
                 }
             }
+            prev=curr;
         }
-        return dp[0][1][k];
+        return prev[1][k];
     }
 };
