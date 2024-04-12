33cc768d80e87859d37540b5edecdfeb89e3c3d5
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-04-12 10:37:13 +0530

    Time: 154 ms (32.48%) | Memory: 13 MB (37.06%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -17,17 +17,32 @@ public:
         int n=nums.size();
         // vector<vector<int>>dp(n,vector<int>(n+1,-1));
         // return solve(nums,0,-1,dp);
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // bottom up
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // for(int ind=n-1;ind>=0;ind--){
+        //     for(int ind1=ind-1;ind1>=-1;ind1--){
+        //         int inc=0;
+        //         int exc=dp[ind+1][ind1+1];
+        //         if(ind1==-1 || nums[ind]>nums[ind1]){
+        //             inc=1+dp[ind+1][ind+1];
+        //         }
+        //         dp[ind][ind1+1]=max(inc,exc);
+        //     }
+        // }
+        // return dp[0][0];
+        // space optimisation
+        vector<int>prev(n+1,0),curr(n+1,0);
         for(int ind=n-1;ind>=0;ind--){
             for(int ind1=ind-1;ind1>=-1;ind1--){
                 int inc=0;
-                int exc=dp[ind+1][ind1+1];
+                int exc=prev[ind1+1];
                 if(ind1==-1 || nums[ind]>nums[ind1]){
-                    inc=1+dp[ind+1][ind+1];
+                    inc=1+prev[ind+1];
                 }
-                dp[ind][ind1+1]=max(inc,exc);
+                curr[ind1+1]=max(inc,exc);
             }
+            prev=curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
