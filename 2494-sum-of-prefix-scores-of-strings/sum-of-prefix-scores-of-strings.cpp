dbcbf7a7867c3ef33d1a1fb817977ab8cff208cf
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-04-16 21:02:35 +0530

    Time: 655 ms (26.67%) | Memory: 726.3 MB (22.33%) - LeetSync

--- 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
@@ -1,96 +1,61 @@
-// class Trie{
-// private:
-//     Trie* arr[26];
-//     int cntpre=0;
+class Trie{
+public:
+    Trie* arr[26];
+    int cntpre=0;
 
-// public:
-// bool check(char ch){
-//     return arr[ch-'a']!=NULL;
-// }
-// Trie*get(char ch){
-//     return arr[ch-'a'];
-// }
-// void put(char ch,Trie*temp){
-//     arr[ch-'a']=temp;
-//     cntpre++;
-// }
-// void incpre(){
-//     cntpre++;
-// }
-// int getpre(){
-//     return cntpre;
-// }
-// };
-class TrieNode {
-    public:
-    TrieNode* children[26];
-    int count=0;
+bool check(char ch){
+    return arr[ch-'a']!=NULL;
+}
+Trie*get(char ch){
+    return arr[ch-'a'];
+}
+void put(char ch,Trie*temp){
+    arr[ch-'a']=temp;
+}
+void incpre(){
+    cntpre++;
+}
+int getpre(){
+    return cntpre;
+}
 };
 class Solution {
-    // private:
-    // Trie*root;
-    // public:
-    // Solution(){
-    //     root=new Trie();
-    // }
-    // void insert(string word){
-    //     Trie*temp=root;
-    //     for(int i=0;i<word.size();i++){
-    //         char ch=word[i];
-    //         if(!temp->check(ch)){
-    //             temp->put(ch,new Trie());
-             
-    //         }
-    //         else{
-    //             temp->incpre();
-    //         }
-    //            cout<<ch<<" "<<temp->getpre()<<endl;
-    //         temp=temp->get(ch);
-    //     }
-    // }
-    // int getsum(string word){
-    //     Trie*temp=root;
-    //     int sum=0;
-    //     for(int i=0;i<word.size();i++){
-    //         char ch=word[i];
-    //         sum+=temp->getpre();
-    //         temp=temp->get(ch);
-    //     }
-    //     return sum;
-    // }
+    private:
+    Trie*root;
     public:
-    void insert(TrieNode* root, string s) {
-        TrieNode* currNode = root;
-        for(int i=0; i<s.size(); i++) {
-            char c = s[i];
-            if(currNode->children[c-'a'] == NULL) currNode->children[c-'a'] = new TrieNode();
-            currNode = currNode->children[c-'a'];
-            currNode->count++;
+    Solution(){
+        root=new Trie();
+    }
+    void insert(string word){
+        Trie*temp=root;
+        for(int i=0;i<word.size();i++){
+            char ch=word[i];
+            if(!temp->check(ch)){
+                temp->put(ch,new Trie());
+            }
+            temp=temp->get(ch);
+            temp->incpre();
         }
     }
-    int getCount(TrieNode* root, string s) {
-        TrieNode* currNode = root;
-        int ans = 0;
-        for(int i=0; i<s.size(); i++) {
-            char c = s[i];
-            if(currNode->children[c-'a'] == NULL) return ans;
-            currNode = currNode->children[c-'a'];
-            ans += currNode->count;
+    int getsum(string word){
+        Trie*temp=root;
+        int sum=0;
+        for(int i=0;i<word.size();i++){
+            char ch=word[i];
+              if(!temp->check(ch))return sum;
+              temp=temp->get(ch);
+              sum+=temp->getpre();
         }
-        return ans;
+        return sum;
     }
     vector<int> sumPrefixScores(vector<string>& words) {
-        TrieNode* root = new TrieNode();
        for(int i=0;i<words.size();i++){
         string res=words[i];
-        insert(root, res);
+        insert(res);
        } 
-    //    int temp=getsum(words[0]);
-    //    int temp1=getsum(words[1]);
-    //    int temp2=getsum(words[2]);
        vector<int>ans;
        for(int i=0;i<words.size();i++){
-           int temp=getCount(root, words[i]);
+           int temp=getsum(words[i]);
            ans.push_back(temp);
        }
        return ans;
