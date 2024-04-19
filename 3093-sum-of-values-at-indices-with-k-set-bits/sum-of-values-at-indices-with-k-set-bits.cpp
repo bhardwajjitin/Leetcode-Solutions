b901fccee8c69f22c1599ca0e77795d2d80f2458
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-04-19 20:53:45 +0530

    Time: 9 ms (56.27%) | Memory: 24.4 MB (58.32%) - LeetSync

--- 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.cpp
+++ 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.cpp
@@ -4,7 +4,7 @@ public:
         int ans=0,n=nums.size();
         for(int i=0;i<n;i++){
             int cnt=0;
-            for(int j=0;j<12;j++){
+            for(int j=0;j<10;j++){
                 if(((1<<j)&i)>0) cnt++;
             }
             if(cnt==k) ans+=nums[i];
