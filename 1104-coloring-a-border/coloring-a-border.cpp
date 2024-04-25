5eb949a1337dcd3231eb2007826e8a8235ee267e
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-04-25 16:44:48 +0530

    Time: 18 ms (16.59%) | Memory: 16.8 MB (39.71%) - LeetSync

---
+++ 1104-coloring-a-border/coloring-a-border.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<int>> colorBorder(vector<vector<int>>& grid, int row, int col, int color) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<pair<int,int>>res;
+        queue<pair<int,int>>q;
+        int di[]={-1,0,0,1};
+        int dj[]={0,-1,1,0};
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        vis[row][col]=1;
+        q.push({row,col});
+        int prev=grid[row][col];
+        while(!q.empty()){
+          int r=q.front().first;
+          int c=q.front().second;
+          q.pop();
+          bool flag=true;
+          for(int i=0;i<4;i++){
+            int nr=r+di[i];
+            int nc=c+dj[i];
+            if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]!=prev){
+               flag=false;
+               break;
+            }
+          }
+          if(r==0 || c==0 || r==m-1 || c==n-1 || flag==false){
+            res.push_back({r,c});
+          }
+          for(int i=0;i<4;i++){
+            int nr=r+di[i];
+            int nc=c+dj[i];
+            if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc] && grid[nr][nc]==prev){
+              vis[nr][nc]=1;
+              q.push({nr,nc});
+            }
+          }
+        }
+        for(int i=0;i<res.size();i++){
+          int r=res[i].first;
+          int c=res[i].second;
+          grid[r][c]=color;
+        }
+        return grid;
+    }
+};
