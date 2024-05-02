ea152ff8b97d37c54540c726b8651462950fc18d
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-05-02 19:31:12 +0530

    Time: 24 ms (33.88%) | Memory: 26.7 MB (7.36%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,21 +1,35 @@
+// class Solution {
+// public:
+//     int findMaxK(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         int i=0;
+//         int j=nums.size()-1;
+//         while(i!=j){
+//           int temp=(-1)*nums[i];
+//           if(temp==nums[j]){
+//             return nums[j];
+//           }
+//           else if(temp>nums[j]){
+//             i++;
+//           }
+//           else{
+//             j--;
+//           }
+//         }
+//         return -1;
+//     }
+// };
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int i=0;
-        int j=nums.size()-1;
-        while(i!=j){
-          int temp=(-1)*nums[i];
-          if(temp==nums[j]){
-            return nums[j];
-          }
-          else if(temp>nums[j]){
-            i++;
-          }
-          else{
-            j--;
-          }
+        int n = nums.size(), ans = -1;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            int alter = nums[i] - (2 * nums[i]);
+            if (mp.find(alter) != mp.end())
+                ans = max(ans, abs(alter));
+            mp[nums[i]]++;
         }
-        return -1;
-    }
+        return ans;
+    }
 };
