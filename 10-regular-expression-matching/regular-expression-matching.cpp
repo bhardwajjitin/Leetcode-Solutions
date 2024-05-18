7550d41dacec5ae65e5b95ff25dfc34275dd6e17
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-05-18 23:10:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (12.68%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,6 +1,6 @@
 class Solution {
   private:
-  bool solve(string&s,string&p,int i,int j){
+  bool solve(string&s,string&p,int i,int j,vector<vector<int>>&dp){
     if(i<0 && j<0)return true;
     if(i<0 && j>=0){
       stack<char>st;
@@ -18,23 +18,26 @@ class Solution {
       return true;
     }
     if(i>=0 && j<0)return false;
+    
+    if(dp[i][j]!=-1)return dp[i][j];
     if(s[i]==p[j] || p[j]=='.'){
-      return solve(s,p,i-1,j-1);
+      return dp[i][j]=solve(s,p,i-1,j-1,dp);
     }
     if(p[j]=='*'){
        if(s[i]==p[j-1] || p[j-1]=='.'){
-        return (solve(s,p,i-1,j)|solve(s,p,i,j-2));
+        return dp[i][j]=(solve(s,p,i-1,j,dp)|solve(s,p,i,j-2,dp));
        }
        else{
-         return solve(s,p,i,j-2);
+         return dp[i][j]=solve(s,p,i,j-2,dp);
        }
     }
-    return false;
+    return dp[i][j]=false;
   }
 public:
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
-        return solve(s,p,n-1,m-1);
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return solve(s,p,n-1,m-1,dp);
     }
 };
