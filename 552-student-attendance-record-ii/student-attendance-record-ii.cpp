0b6f3ac6f6c28a5ac82678948f8c4d65da5831ee
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-05-27 23:04:38 +0530

    Time: 170 ms (67.03%) | Memory: 7.3 MB (92.91%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -22,28 +22,49 @@ public:
     int checkRecord(int n) {
         // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(3,vector<int>(4,-1)));
         // return solve(n,0,0,dp)%mod;
-// tabulation
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(3,vector<int>(4,1)));
-        for(int i=0;i<=n;i++){
-          for(int j=0;j<2;j++){
-            dp[i][j][3]=0;
-          }
+// // tabulation
+        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(3,vector<int>(4,1)));
+        // for(int i=0;i<=n;i++){
+        //   for(int j=0;j<2;j++){
+        //     dp[i][j][3]=0;
+        //   }
+        // }
+        // for(int i=0;i<=n;i++){
+        //   for(int j=0;j<3;j++){
+        //     dp[i][2][j]=0;
+        //   }
+        // }
+        // for(int i=1;i<=n;i++){
+        //   for(int a=0;a<=1;a++){
+        //     for(int l=0;l<=2;l++){
+        //       long long first=dp[i-1][a+1][0];
+        //       long long second=dp[i-1][a][l+1];
+        //       long long third=dp[i-1][a][0];
+        //       dp[i][a][l]=(first+second+third)%mod;
+        //     }
+        //   }
+        // }
+        // return dp[n][0][0];
+
+        // space optimisation
+        vector<vector<int>>prev(3,vector<int>(4,1)),curr(3,vector<int>(4,0));
+        for(int j=0;j<2;j++){
+            prev[j][3]=0;
         }
-        for(int i=0;i<=n;i++){
-          for(int j=0;j<3;j++){
-            dp[i][2][j]=0;
-          }
+        for(int j=0;j<3;j++){
+            prev[2][j]=0;
         }
         for(int i=1;i<=n;i++){
           for(int a=0;a<=1;a++){
             for(int l=0;l<=2;l++){
-              long long first=dp[i-1][a+1][0];
-              long long second=dp[i-1][a][l+1];
-              long long third=dp[i-1][a][0];
-              dp[i][a][l]=(first+second+third)%mod;
+              long long first=prev[a+1][0];
+              long long second=prev[a][l+1];
+              long long third=prev[a][0];
+              curr[a][l]=(first+second+third)%mod;
             }
           }
+          prev=curr;
         }
-        return dp[n][0][0];
+        return prev[0][0];
     }
 };
