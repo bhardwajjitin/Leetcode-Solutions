07290d5cca6d4bbe1dff18f40ec15f74a88482b6
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-06-18 11:51:06 +0530

    Time: 99 ms (22.81%) | Memory: 45.2 MB (20.49%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& diff, vector<int>& profit, vector<int>& worker) {
+        unordered_map<int,int>umap;
+        for(int i=0;i<diff.size();i++){
+          umap[diff[i]]=max(umap[diff[i]],profit[i]);
+        }
+        sort(diff.begin(),diff.end());
         int ans=0;
+        vector<long long>res(diff.size()+10);
+        res[0]=umap[diff[0]];
+        for(int i=1;i<diff.size();i++){
+          res[i]=max(res[i-1],(long long)umap[diff[i]]);
+        }
         for(int i=0;i<worker.size();i++){
-          int maxi=INT_MIN;
-          for(int j=0;j<diff.size();j++){
-            if(diff[j]<=worker[i]){
-              maxi=max(maxi,profit[j]);
-            }
+          int temp=worker[i];
+          int ind=upper_bound(diff.begin(),diff.end(),temp)-diff.begin();
+          if(ind!=0){
+          ans+=res[ind-1];
           }
-          if(maxi!=INT_MIN)
-          ans+=maxi;
         }
         return ans;
     }
