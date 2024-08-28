38e7d85262c66cfbbf8c17b4d0ec28a73c4aed9d
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-08-28 18:31:40 +0530

    Time: 21 ms (85.14%) | Memory: 12.5 MB (40.61%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -16,21 +16,21 @@ public:
         cuts.insert(cuts.begin(),0);
         sort(cuts.begin(),cuts.end());
         int c=cuts.size();
-        vector<vector<int>>dp(c+1,vector<int>(c+1,-1));
-        return solve(cuts,1,cuts.size()-2,dp);
-        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // vector<vector<int>>dp(c+1,vector<int>(c+1,-1));
+        // return solve(cuts,1,cuts.size()-2,dp);
+        vector<vector<int>>dp(c+1,vector<int>(c+1,0));
 
-        // for(int i=cuts.size()-1;i>=1;i--){
-        //     for(int j=1;j<cuts.size()-1;j++){
-        //         if(i>j)continue;
-        //         int mini=INT_MAX;
-        //         for(int k=i;k<=j;k++){
-        //             int cost=cuts[j+1]-cuts[i-1]+dp[i][k-1]+dp[k+1][j];
-        //             mini=min(mini,cost);
-        //         }
-        //         dp[i][j]=mini;
-        //     }
-        // }
-        // return dp[1][cuts.size()-2];
+        for(int i=cuts.size()-1;i>=1;i--){
+            for(int j=1;j<cuts.size()-1;j++){
+                if(i>j)continue;
+                int mini=INT_MAX;
+                for(int k=i;k<=j;k++){
+                    int cost=cuts[j+1]-cuts[i-1]+dp[i][k-1]+dp[k+1][j];
+                    mini=min(mini,cost);
+                }
+                dp[i][j]=mini;
+            }
+        }
+        return dp[1][cuts.size()-2];
     }
 };
