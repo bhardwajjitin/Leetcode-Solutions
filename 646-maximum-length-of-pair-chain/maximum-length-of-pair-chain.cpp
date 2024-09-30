fa4d4b5976d573490b6bd7f024de0128cfd5c6da
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-09-30 10:01:52 +0530

    Time: 40 ms (72.68%) | Memory: 26.3 MB (94.22%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -11,22 +11,33 @@ class Solution {
     }
 public:
     int findLongestChain(vector<vector<int>>& pairs) {
-        sort(pairs.begin(),pairs.end());
+        // sort(pairs.begin(),pairs.end());
         // return solve(pairs,0,-1);
 
         // tabulation
         int n=pairs.size();
-        vector<vector<int>>dp(pairs.size()+1,vector<int>(pairs.size()+1,0));
-        for(int i=n-1;i>=0;i--){
-            for(int j=-1;j<n;j++){
-                int notpick=dp[i+1][j+1];
-                int pick=0;
-                if(j==-1 || pairs[j][1]<pairs[i][0]){
-                pick=1+dp[i+1][i+1];
-                }
-                dp[i][j+1]=max(pick,notpick);
-            }
+        // vector<vector<int>>dp(pairs.size()+1,vector<int>(pairs.size()+1,0));
+        // for(int i=n-1;i>=0;i--){
+        //     for(int j=-1;j<n;j++){
+        //         int notpick=dp[i+1][j+1];
+        //         int pick=0;
+        //         if(j==-1 || pairs[j][1]<pairs[i][0]){
+        //         pick=1+dp[i+1][i+1];
+        //         }
+        //         dp[i][j+1]=max(pick,notpick);
+        //     }
+        // }
+        // return dp[0][0];
+        // greedy solution
+    sort(pairs.begin(),pairs.end(),[](const vector<int>& a,const vector<int>& b) {return a[1]<b[1];});
+     int cnt=1;
+     int last=pairs[0][1];
+     for(int i=1;i<n;i++){
+        if(pairs[i][0]>last){
+            last=pairs[i][1];
+            cnt++;
         }
-        return dp[0][0];
+     }
+     return cnt;
     }
 };
