c4109ab6c31f9c2bf632f9fcc067978d5785b7cd
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-10-19 22:34:23 +0530

    Time: 1293 ms (16.52%) | Memory: 399.4 MB (42.01%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -1,70 +1,24 @@
 class Solution {
-  int mod=1e9+7;
-  private:
-    int solve(int n,int a,int l,vector<vector<vector<int>>>&dp){
-      if(a>=2){
-        a=0;
-        return 0;
-      }
-      if(l>=3){
-        return 0;
-      }
-      if(n==0){
-        return 1;
-      }
-      if(dp[n][a][l]!=-1)return dp[n][a][l];
-      long long first=solve(n-1,a+1,0,dp);
-      long long second=solve(n-1,a,l+1,dp);
-      long long third=solve(n-1,a,0,dp);
-      return dp[n][a][l]=(first+second+third)%mod;
+    private:
+    int solve(int n,int cnta,int cntl,vector<vector<vector<int>>>&dp){
+        if(n==0){
+            if(cnta<2)return 1;
+            return 0;
+        }
+        if(dp[n][cnta][cntl]!=-1)return dp[n][cnta][cntl];
+        int absent=0,leave=0,present=0;
+        present=solve(n-1,cnta,0,dp);
+        if(cnta<1){
+            absent=solve(n-1,cnta+1,0,dp);
+        }
+        if(cntl<2){
+            leave=solve(n-1,cnta,cntl+1,dp);
+        }
+        return dp[n][cnta][cntl]=((long long)present+(long long)absent+(long long)leave)%1000000007;
     }
 public:
     int checkRecord(int n) {
-        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(3,vector<int>(4,-1)));
-        // return solve(n,0,0,dp)%mod;
-// // tabulation
-        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(3,vector<int>(4,1)));
-        // for(int i=0;i<=n;i++){
-        //   for(int j=0;j<2;j++){
-        //     dp[i][j][3]=0;
-        //   }
-        // }
-        // for(int i=0;i<=n;i++){
-        //   for(int j=0;j<3;j++){
-        //     dp[i][2][j]=0;
-        //   }
-        // }
-        // for(int i=1;i<=n;i++){
-        //   for(int a=0;a<=1;a++){
-        //     for(int l=0;l<=2;l++){
-        //       long long first=dp[i-1][a+1][0];
-        //       long long second=dp[i-1][a][l+1];
-        //       long long third=dp[i-1][a][0];
-        //       dp[i][a][l]=(first+second+third)%mod;
-        //     }
-        //   }
-        // }
-        // return dp[n][0][0];
-
-        // space optimisation
-        vector<vector<int>>prev(3,vector<int>(4,1)),curr(3,vector<int>(4,0));
-        for(int j=0;j<2;j++){
-            prev[j][3]=0;
-        }
-        for(int j=0;j<3;j++){
-            prev[2][j]=0;
-        }
-        for(int i=1;i<=n;i++){
-          for(int a=0;a<=1;a++){
-            for(int l=0;l<=2;l++){
-              long long first=prev[a+1][0];
-              long long second=prev[a][l+1];
-              long long third=prev[a][0];
-              curr[a][l]=(first+second+third)%mod;
-            }
-          }
-          prev=curr;
-        }
-        return prev[0][0];
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
+        return solve(n,0,0,dp);
     }
 };
