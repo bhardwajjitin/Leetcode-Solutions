ce9a776d6aacd0a7c1ab92150f91a88d9dd3d4a0
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-11-06 10:42:42 +0530

    Time: 4 ms (34.53%) | Memory: 31.2 MB (86.44%) - LeetSync

---
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int countsetbits(int number){
+        int cnt=0;
+        while(number){
+            cnt+=number&1;
+            number>>=1;
+        }
+        return cnt;
+    }
+public:
+    bool canSortArray(vector<int>& nums) {
+        for (int times = 0; times < nums.size(); times++) {
+            int i = 0;
+            int j = 1;
+            while (j < nums.size()) {
+                if (nums[i] > nums[j]) {
+                    if(countsetbits(nums[i]) == countsetbits(nums[j]))
+                    swap(nums[i], nums[j]);
+                    else
+                    return false;
+                }
+                i++;
+                j++;
+            }
+        }
+        return true;
+    }
+};
