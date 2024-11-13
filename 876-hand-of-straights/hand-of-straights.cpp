e425b83d450bf7abc0ded4b6bf99c4193858db58
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-11-13 23:51:25 +0530

    Time: 68 ms (11.75%) | Memory: 39.3 MB (5.02%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        multiset<int>ml;
-        for(auto it:hand){
-            ml.insert(it);
-        }
-        while(!ml.empty()){
-          int node=*ml.begin();
-          auto it=ml.find(node);
-          if(it!=ml.end()){
-            ml.erase(it);
-          } 
-          int cnt=1;
-          while(cnt<groupSize){
-            auto it=ml.find(node+1);
-            if(it==ml.end())return false;
-            if(it!=ml.end()){
-              ml.erase(it);
-              node++;
-              cnt++;
+         set<int>ml;
+         if(hand.size()%groupSize!=0)return false;
+         unordered_map<int,int>umap;
+         for(int i=0;i<hand.size();i++){
+            ml.insert(hand[i]);
+            umap[hand[i]]++;
+         }
+         int k=groupSize;
+         while(!ml.empty()){
+            int top=*ml.begin();
+            umap[top]--;
+            if(umap[top]==0){
+                ml.erase(top);
+                umap.erase(top);
             }
-          }
-        }
-        return true;
+            k-=1;
+            while(!ml.empty() && k!=0){
+            if(ml.find(top+1)!=ml.end()){
+                umap[top+1]--;
+                if(umap[top+1]==0){
+                    ml.erase(top+1);
+                    umap.erase(top+1);
+                }
+                top++;
+            }
+            else{
+                return false;
+            }
+            k--;
+            }
+            k=groupSize;
+         }
+         return true;
     }
 };
