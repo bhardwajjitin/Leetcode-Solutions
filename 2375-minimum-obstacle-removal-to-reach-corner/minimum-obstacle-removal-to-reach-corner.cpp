106ecc24af00bad1dae7b576afc610509942ab5f
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-11-28 23:49:31 +0530

    Time: 260 ms (72.53%) | Memory: 109.8 MB (69.12%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,35 +1,25 @@
 class Solution {
+    int dr[4] = {0,1,0,-1} , dc[4] = {1,0,-1,0};
 public:
     int minimumObstacles(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>>vis(m,vector<int>(n,0));
-        set<pair<int,pair<int,int>>>pq;
-        pq.insert({0,{0,0}});
-        vis[0][0]=1;
-        int di[]={-1,0,0,1};
-        int dj[]={0,-1,1,0};
-        while(!pq.empty()){
-            auto it=*pq.begin();
-            int dis=it.first;
-            int row=it.second.first;
-            int col=it.second.second;
-            pq.erase(*pq.begin());
-            vis[row][col]=1;
-            if(row==m-1 && col==n-1)return dis;
-            for(int i=0;i<4;i++){
-                int nrow=row+di[i];
-                int ncol=col+dj[i];
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !vis[nrow][ncol]){
-                    if(grid[nrow][ncol]==1){
-                        pq.insert({dis+1,{nrow,ncol}});
-                    }
-                    else{
-                        pq.insert({dis,{nrow,ncol}});
-                    }
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>minh;
+        vector<vector<int>>vis(grid.size(),vector<int>(grid[0].size(),0));
+        int start = 0;
+        if(grid[0][0] == 1)start = 1;
+        minh.push({start,0,0});
+        while(!minh.empty()){
+            auto [currObstacles,currRow,currCol] = minh.top();
+            minh.pop();
+            if(currRow == grid.size()-1 and currCol == grid[0].size()-1)return currObstacles;
+            for(int delta = 0 ; delta < 4; delta++){
+                int nRow = currRow+dr[delta] , nCol = currCol+dc[delta];
+                if(nRow >= 0 and nCol >= 0 and nRow < grid.size() and nCol < grid[0].size() and vis[nRow][nCol] == 0){
+                    vis[nRow][nCol] = 1;
+                    int newObs = currObstacles + grid[nRow][nCol];
+                    minh.push({newObs,nRow,nCol});
                 }
             }
         }
-        return -1;
-    }
+        return 0;
+    }
 };
