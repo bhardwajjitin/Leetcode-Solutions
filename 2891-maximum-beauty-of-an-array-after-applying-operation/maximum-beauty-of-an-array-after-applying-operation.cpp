a2d26f06032716cab437a5439ec9917a934f442f
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-12-12 01:02:21 +0530

    Time: 243 ms (32.03%) | Memory: 123.5 MB (37.98%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,17 +1,58 @@
 class Solution {
-public:
-    int maximumBeauty(vector<int>& nums, int k) {
-        map<int,int>mp;
-        for(auto it:nums){
-            mp[it-k]++;
-            mp[it+k+1]--;
-        }
-        int ans=0;
-        int sum=0;
-        for(auto it:mp){
-            sum+=it.second;
-            ans=max(ans,sum);
+    private:
+    int blackbox(vector<int>&res,int val){
+        int low=0;
+        int high=res.size()-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(res[mid]<=val){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
         return ans;
+    }
+    int whitebox(vector<int>&res,int val){
+        int low=0;
+        int high=res.size()-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(res[mid]>=val){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        vector<int>lower_limit,upper_limit;
+        for(int i=0;i<nums.size();i++){
+            int lower=nums[i]-k;
+            int higher=nums[i]+k;
+            lower_limit.push_back(lower);
+            upper_limit.push_back(higher);
+        }
+        int maxi=0;
+        int low=lower_limit[0];
+        int high=upper_limit[upper_limit.size()-1];
+        for(int i=low;i<=high;i++){
+            int getlowind=blackbox(lower_limit,i);
+            int gethighind=whitebox(upper_limit,i);
+            if(getlowind>=gethighind){
+                int temp=getlowind-gethighind+1;
+                maxi=max(maxi,temp);
+            }
+        }
+        return maxi;
     }
 };
