6b2f4ed451e42aabbeb31172e246e0f14062da9e
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-12-13 01:03:17 +0530

    Time: 849 ms (5.05%) | Memory: 509.1 MB (5.01%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -1,38 +1,57 @@
 class Solution {
 public:
+    tuple<long long,long long,bool> getTheFirstOneFree(vector<long long>&freeAt, int neededAt){
+        long long ansRoom = 200 , gotFreeAt = 0;
+        for(int room = 0 ; room < freeAt.size() ; room++){
+            if(freeAt[room] <= neededAt){
+                ansRoom = room;
+                gotFreeAt = freeAt[room];
+                return {ansRoom,gotFreeAt,1};
+            }
+        }
+        return {ansRoom,gotFreeAt,0};
+    }
+    
     int mostBooked(int n, vector<vector<int>>& meetings) {
+        vector<long long>freeAt(n,0);
+        unordered_map<int,int>usedFrequency;
+        priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>>minh;
+        for(int i = 0 ; i < n ; i++)minh.push({0,i});
         sort(meetings.begin(),meetings.end());
-        map<int,int>umap;
-        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<>>pq;
-        for(int i=0;i<n;i++){
-            pq.push({0,i});
-        }
-        vector<int>empty;
-        for(auto it:meetings){
-            long long start=it[0];
-            long long end=it[1];
-            while(!pq.empty() && pq.top().first<=start){
-                int room=pq.top().second;
-                pq.pop();
-                empty.push_back(room);
+
+        for(int index = 0 ; index < meetings.size() ; index++){
+            long long startTime = meetings[index][0] , endTime = meetings[index][1];
+            long long meetingDuration = endTime-startTime;
+            auto [roomNumber,gotFreeAt,flag] = getTheFirstOneFree(freeAt,startTime);
+            if(flag){
+                usedFrequency[roomNumber]+=1;
+                long long newFreeTime = max(meetingDuration+gotFreeAt,endTime);
+                freeAt[roomNumber] = newFreeTime;
+                minh.push({newFreeTime,roomNumber});
             }
-            for(auto it:empty){
-                pq.push({0,it});
-            }
-            empty.clear();
-            long long newtime=pq.top().first==0?end:pq.top().first+(end-start);
-            int room=pq.top().second;
-            umap[room]++;
-            pq.pop();
-            pq.push({newtime,room});
-        }
-        int ans=0,maxi=INT_MIN;
-        for(auto it:umap){
-            if(it.second>maxi){
-                ans=it.first;
-                maxi=it.second;
+            else{
+                priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>>newMinH;
+                for(int i = 0 ; i < n ; i++)newMinH.push({freeAt[i],i});
+                auto [freeNow,currentBestRoom] = newMinH.top();
+                newMinH.pop();
+                usedFrequency[currentBestRoom]+=1;
+                long long newFreeTime = max(meetingDuration+freeNow,endTime);
+                freeAt[currentBestRoom] = newFreeTime;
+                minh.push({newFreeTime,currentBestRoom});
             }
         }
-        return ans;
+
+        int maxUsedCount = 0 , maxUsedRoom = 200;
+        for(auto i : usedFrequency){
+            // cout<<i.first<<" , "<<i.second<<endl;
+            if(i.second >= maxUsedCount){
+                maxUsedCount = i.second;
+            }
+        }
+        // cout<<"usedMaxCount = "<<maxUsedCount<<endl;
+        for(auto i : usedFrequency){
+            if(maxUsedCount == i.second)maxUsedRoom = min(maxUsedRoom,i.first);
+        }
+        return maxUsedRoom;
     }
 };
