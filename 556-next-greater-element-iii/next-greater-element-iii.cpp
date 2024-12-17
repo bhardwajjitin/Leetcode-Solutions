e60b6d33d1d4cc8cf3b88718ea0308b3a1faef30
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-12-17 16:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (6.36%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
     int nextGreaterElement(int n) {
-        vector<int>ans;
-        int temp=n;
-        while(temp!=0){
-          int r=temp%10;
-          ans.push_back(r);
-          temp/=10;
+        vector<int> arr;
+        while (n != 0) {
+            int r = n % 10;
+            arr.push_back(r);
+            n /= 10;
         }
-        reverse(ans.begin(),ans.end());
-        next_permutation(ans.begin(),ans.end());
-        long long  sum=0;
-        for(int i=0;i<ans.size();i++){
-          sum=sum*10+ans[i];
+        reverse(arr.begin(), arr.end());
+        bool flag = false;
+        for (int i = arr.size() - 2; i >= 0; i--) {
+            int curr = arr[i];
+            int swapind = -1;
+            int mini = INT_MAX;
+            for (int j = i + 1; j < arr.size(); j++) {
+                if (arr[i] < arr[j] && mini > arr[j]) {
+                    swapind = j;
+                    mini = arr[j];
+                }
+            }
+            if (swapind != -1) {
+                flag = true;
+                swap(arr[i], arr[swapind]);
+                sort(arr.begin() + i + 1, arr.end());
+                break;
+            }
         }
-        if(sum>INT_MAX || sum<=n)return -1;
-        return sum;
+        int number = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (number > (INT_MAX - arr[i]) / 10) {
+                return -1;
+            }
+            number = number * 10 + arr[i];
+        }
+        return flag == false ? -1 :number;
     }
 };
