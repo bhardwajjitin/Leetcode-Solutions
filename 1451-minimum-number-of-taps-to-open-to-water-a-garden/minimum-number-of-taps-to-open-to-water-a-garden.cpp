96954bda82cb8a5889981fb2517b1097179485f1
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2024-12-18 01:01:21 +0530

    Time: 4 ms (49.61%) | Memory: 20 MB (21.79%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
-        vector<pair<int,int>>arr;
-        for(int i=0;i<ranges.size();i++){
-            int start=max(i-ranges[i],0);
-            int end=min(i+ranges[i],n);
-            if(start==end)continue;
-            arr.push_back({start,end});
+        vector<pair<int, int>> arr;
+        for (int i = 0; i < ranges.size(); i++) {
+            int start = max(0, i - ranges[i]);
+            int end = min(i + ranges[i], n);
+            arr.push_back({start, end});
         }
-        sort(arr.begin(),arr.end());
-        int cnt=0;
-        int start=0;
-        int last=0;
-        int i=0;
-        while(start<n) {
-            while (i<arr.size() && arr[i].first<=start) {
-                last=max(last,arr[i].second);
+        sort(arr.begin(), arr.end());
+        int lastCovered = 0;
+        int nextCovered = 0;
+        int i = 0, cnt = 0;
+
+        while (lastCovered < n) {
+            while (i < arr.size() && arr[i].first <= lastCovered) {
+                nextCovered = max(nextCovered, arr[i].second);
                 i++;
             }
-            if (last==start) return -1;
-            start=last;
+
+            if (nextCovered <= lastCovered) {
+                return -1;
+            }
+            lastCovered = nextCovered;
             cnt++;
         }
-
         return cnt;
     }
 };
