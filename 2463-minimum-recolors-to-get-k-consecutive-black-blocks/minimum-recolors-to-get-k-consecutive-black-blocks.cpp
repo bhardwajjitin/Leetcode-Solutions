3ff3a8e248970fd51fda30211067896056fa9cf3
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2025-03-09 00:05:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (5.10%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int minimumRecolors(string blocks, int k) {
-        int low=k;
-        int high=blocks.size();
-        int mini=INT_MAX;
-        for(int size=low;size<=high;size++){
-           int left=0;
-           int right=size-1;
-           while(right<blocks.size()){
-              if(right-left+1<size){
-                right++;
-              }
-              else if(right-left+1==size){
-                int cnt=0;
-                for(int i=left;i<=right;i++){
-                    if(blocks[i]=='W')cnt++;
-                }
-                mini=min(cnt,mini);
-                right++;
-                left++;
-              }
-           }
+        vector<int> pre(blocks.size(), 0);
+        int prev = 0;
+        int mini = INT_MAX;
+        for (int i = 0; i < blocks.size(); i++) {
+            if (blocks[i] == 'W') {
+                pre[i] = prev + 1;
+                prev = pre[i];
+            } else {
+                pre[i] = prev;
+            }
+        }
+        for (int i = k-1; i < pre.size(); i++) {
+            int right = pre[i];
+            int left = (i - k>= 0) ? pre[i - k] : 0;
+            int cnt = right - left;
+            mini = min(cnt, mini);
         }
         return mini;
     }
