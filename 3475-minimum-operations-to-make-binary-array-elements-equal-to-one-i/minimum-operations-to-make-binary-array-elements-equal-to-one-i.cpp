047a72d7368390d736a8c2b79c80452832fc6c3d
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2025-03-19 20:38:10 +0530

    Time: 1 ms (92.65%) | Memory: 140 MB (47.55%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,39 +1,20 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int i=0;
-        int j=0;
         int cnt=0;
-        if(nums[i]!=0){
-                while(i<nums.size() && nums[i]==1){
-                    i++;
-                    j++;
+        int i=0;
+        while(i<nums.size()-2){
+            if(nums[i]==0){
+                nums[i]=!nums[i];
+                nums[i+1]=!nums[i+1];
+                nums[i+2]=!nums[i+2];
+                cnt++;
             }
+            i++;
         }
-        while(j<nums.size()){
-            if(j-i+1<3){
-                j++;
-            }
-            else if(j-i+1==3){
-                bool flag=false;
-                for(int k=i;k<=j;k++){
-                    if(nums[k]==0)flag=true;
-                    nums[k]=!nums[k];
-                }
-                if(flag)cnt++;
-                if(nums[i]!=0){
-                while(i<nums.size() && nums[i]==1){
-                    i++;
-                    j++;
-                }
-                }
-                else{
-                    i++;
-                    j++;
-                }
-            }
+        for(auto it:nums){
+            if(it==0)return -1;
         }
-        for(auto it:nums)if(it==0)return -1;
         return cnt;
     }
 };
