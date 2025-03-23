e0aede6afb53f99b0530d6a48b9ec0231cb14285
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2025-03-23 09:19:31 +0530

    Time: 6 ms (76.13%) | Memory: 37.7 MB (43.81%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,37 +1,35 @@
 class Solution {
-    #define mod 1000000007
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<pair<long long,long long>>adjlist[n];
-        for(int i=0;i<roads.size();i++){
-            long long u=roads[i][0];
-            long long v=roads[i][1];
-            long long dis=roads[i][2];
-            adjlist[u].push_back({v,dis});
-            adjlist[v].push_back({u,dis});
-        }
-        priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>>pq;
-        vector<long long>dist(n,LLONG_MAX),ways(n,1);
-        dist[0]=0;
-        ways[0]=1;
+        const int mod=1000000007;
+        vector<pair<long long,int>>res(n,{LLONG_MAX,0});
+        priority_queue<tuple<long long,int>,vector<tuple<long long,int>>,greater<>>pq;
         pq.push({0,0});
+        vector<pair<int,int>>adjlist[n];
+        res[0]={0,1};
+        for(auto temp:roads){
+            int u=temp[0];
+            int v=temp[1];
+            int wt=temp[2];
+            adjlist[u].push_back({v,wt});
+            adjlist[v].push_back({u,wt});
+        }
         while(!pq.empty()){
-            long long dis=pq.top().first;
-            long long node=pq.top().second;
+            auto [dist,node]=pq.top();
             pq.pop();
             for(auto it:adjlist[node]){
-                long long adjnode=it.first;
-                long long edw=it.second;
-                if(dist[adjnode]>dis+edw){
-                    dist[adjnode]=dis+edw;
-                    pq.push({dis+edw,adjnode});
-                    ways[adjnode]=ways[node];
+                int adjnode=it.first;
+                long long wt=it.second;
+                if(res[adjnode].first>dist+wt){
+                    res[adjnode].second=res[node].second%mod;
+                    res[adjnode].first=dist+wt;
+                    pq.push({res[adjnode].first,adjnode});
                 }
-                else if(dist[adjnode]==dis+edw){
-                    ways[adjnode]+=(ways[node])%mod;
-                }
+                else if(res[adjnode].first==dist+wt){
+                    res[adjnode].second=(res[adjnode].second+res[node].second)%mod;
+                } 
             }
         }
-        return ways[n-1]%mod;
+        return res[n-1].second%mod;
     }
 };
