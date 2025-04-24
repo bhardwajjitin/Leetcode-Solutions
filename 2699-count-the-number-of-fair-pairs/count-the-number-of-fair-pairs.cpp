f644dbb6c41cff910c32080b31ad5f92b1164715
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2025-04-24 22:25:48 +0530

    Time: 70 ms (7.01%) | Memory: 71.8 MB (5.35%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -1,48 +1,39 @@
 class Solution {
-    private:
-    int getlower(vector<int>&nums,int&anslow,int low,int l){
-        int h=nums.size()-1;
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            if(nums[mid]>=low){
-                anslow=mid;
-                h=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return anslow;
-    }
-    int gethigher(vector<int>&nums,int&ansup,int high,int l){
-        int h=nums.size()-1;
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            if(nums[mid]<=high){
-                ansup=mid;
-                l=mid+1;
-            }
-            else{
-                h=mid-1;
-            }
-        }
-        return ansup;
-    }
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
         sort(nums.begin(),nums.end());
-        long long cnt=0;
+        long long answer=0;
         for(int i=0;i<nums.size();i++){
             int low=lower-nums[i];
-            int upp=upper-nums[i];
-            int anslow=-1;
-            int ansup=-1;
-            getlower(nums,anslow,low,i+1);
-            gethigher(nums,ansup,upp,i+1);
-            if(ansup!=-1 && anslow!=-1){
-            cnt+=(ansup-anslow)+1;
+            int high=upper-nums[i];
+            int l=i+1,h=nums.size()-1;
+            int stind=-1,endind=-1;
+            
+            while(l<=h){
+                int mid=l+(h-l)/2;
+                if(nums[mid]>=low){
+                    stind=mid;
+                    h=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
             }
+            l=i+1,h=nums.size()-1;
+            while(l<=h){
+                int mid=l+(h-l)/2;
+                if(nums[mid]<=high){
+                    endind=mid;
+                    l=mid+1;
+                }
+                else{
+                    h=mid-1;
+                }
+            }
+            if(stind!=-1 && endind!=-1){
+                answer+=endind-stind+1;
+            }     
         }
-        return cnt;
+        return answer;
     }
 };
