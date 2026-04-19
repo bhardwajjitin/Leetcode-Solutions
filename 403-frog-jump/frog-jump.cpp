82bd0599a6536653cd9751b25c0d506a63cbb175
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2026-04-19 22:20:24 +0530

    Time: 79 ms (69.93%) | Memory: 214.6 MB (27.87%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -1,34 +1,46 @@
 class Solution {
+private:
+    int search_index(vector<int>& stones, int dist) {
+        int low = 0, high = stones.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (stones[mid] == dist) return mid;
+            else if (stones[mid] > dist) high = mid - 1;
+            else low = mid + 1;
+        }
+        return -1;
+    }
+
+    bool solve(vector<int>& stones, int index, int curr_k, vector<vector<int>>& dp) {
+        if (index == stones.size() - 1) return true;
+
+        if (dp[index][curr_k] != -1) return dp[index][curr_k];
+
+        for (int jump = curr_k - 1; jump <= curr_k + 1; jump++) {
+            if (jump <= 0) continue;
+
+            int next_dist = stones[index] + jump;
+            int next_index = search_index(stones, next_dist);
+
+            if (next_index != -1) {
+                if (solve(stones, next_index, jump, dp)) {
+                    return dp[index][curr_k] = true;
+                }
+            }
+        }
+
+        return dp[index][curr_k] = false;
+    }
+
 public:
     bool canCross(vector<int>& stones) {
-      if(stones[1]!=1)return false;
-        unordered_map<int,int>umap;
-        for(int i=0;i<stones.size();i++){
-          umap[stones[i]]=i;
-        }
-        set<pair<int,int>>q;
-        q.insert({1,1});
-        while(!q.empty()){
-           auto it=*q.begin();
-           q.erase(*q.begin());
-           int ind=it.first;
-           int jump=it.second;
-           if(ind==stones.size()-1){
-            return true;
-           }
-           if(umap.find(stones[ind]+jump-1)!=umap.end() && umap[stones[ind]+jump-1]>ind){
-            int index=umap[stones[ind]+jump-1];
-            q.insert({index,jump-1});
-           }
-           if(umap.find(stones[ind]+jump)!=umap.end() && umap[stones[ind]+jump]>ind){
-            int index=umap[stones[ind]+jump];
-            q.insert({index,jump});
-           }
-           if(umap.find(stones[ind]+jump+1)!=umap.end() && umap[stones[ind]+jump+1]>ind){
-            int index=umap[stones[ind]+jump+1];
-            q.insert({index,jump+1});
-           }
-        }
-        return false;
+        int n = stones.size();
+
+        if (stones[1] - stones[0] > 1) return false;
+
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+
+        return solve(stones, 1, 1, dp);
     }
 };
