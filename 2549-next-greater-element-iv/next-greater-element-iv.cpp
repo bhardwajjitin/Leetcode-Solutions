132f4ac48132019a940c5d2f1f782a3d6aca9657
Jitin Bhardwaj <118838837+bhardwajjitin@users.noreply.github.com>
2026-05-29 08:07:44 +0530

    Time: 60 ms (42.22%) | Memory: 94.6 MB (84.44%) - LeetSync

--- 2549-next-greater-element-iv/next-greater-element-iv.cpp
+++ 2549-next-greater-element-iv/next-greater-element-iv.cpp
@@ -1,60 +1,60 @@
 class Solution {
-private:
-    void getinsert(vector<int>& nums, vector<int>& arr, int ind) {
-        int low = 0;
-        int high = arr.size() - 1;
+// private:
+    // void getinsert(vector<int>& nums, vector<int>& arr, int ind) {
+    //     int low = 0;
+    //     int high = arr.size() - 1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+    //     while (low <= high) {
+    //         int mid = low + (high - low) / 2;
 
-            if (nums[arr[mid]] >= nums[ind]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
+    //         if (nums[arr[mid]] >= nums[ind]) {
+    //             high = mid - 1;
+    //         } else {
+    //             low = mid + 1;
+    //         }
+    //     }
 
-        arr.insert(arr.begin() + low, ind);
-    }
+    //     arr.insert(arr.begin() + low, ind);
+    // }
 
-    int check(vector<int>& arr, int ele, vector<int>& nums) {
-        int low = 0;
-        int high = arr.size() - 1;
+    // int check(vector<int>& arr, int ele, vector<int>& nums) {
+    //     int low = 0;
+    //     int high = arr.size() - 1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+    //     while (low <= high) {
+    //         int mid = low + (high - low) / 2;
 
-            if (nums[arr[mid]] >= ele) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
+    //         if (nums[arr[mid]] >= ele) {
+    //             high = mid - 1;
+    //         } else {
+    //             low = mid + 1;
+    //         }
+    //     }
 
-        return high;
-    }
+    //     return high;
+    // }
 
 public:
     vector<int> secondGreaterElement(vector<int>& nums) {
         stack<int> st1;
-        vector<int> ans(nums.size(), -1), arr;
+        vector<int>ans(nums.size(), -1);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
 
         for (int i = 0; i < nums.size(); i++) {
             int ele = nums[i];
-
-            int index = check(arr, ele, nums);
-
-            for (int j = 0; j <= index; j++) {
-                ans[arr[j]] = ele;
-            }
-
-            if (index >= 0) {
-                arr.erase(arr.begin(), arr.begin() + index + 1);
+            while(!pq.empty()){
+                if(nums[pq.top().second]<ele){
+                    ans[pq.top().second]=ele;
+                    pq.pop();
+                }
+                else{
+                    break;
+                }
             }
 
             while (!st1.empty()) {
                 if (nums[i] > nums[st1.top()]) {
-                    getinsert(nums, arr, st1.top());
+                    pq.push({nums[st1.top()],st1.top()});
                     st1.pop();
                 } else {
                     break;
